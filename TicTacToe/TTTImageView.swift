277f31e2f181a86fc74cc3d1891499dabafb63b3
Abhishek Bansal <abbansal1995@gmail.com>
2019-07-18 20:57:32 -0400

    Creating new UIImageView class

---
+++ TicTacToe/TTTImageView.swift
@@ -0,0 +1,21 @@
+//
+//  TTTImageView.swift
+//  TicTacToe
+//
+//  Created by Abhishek Bansal on 2019-07-18.
+//  Copyright © 2019 Abhishek Bansal. All rights reserved.
+//
+
+import UIKit
+
+class TTTImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
