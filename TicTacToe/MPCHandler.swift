4e622f377158a849c8eb7dec79bca915126eab9d
Abhishek Bansal <abbansal1995@gmail.com>
2019-07-18 21:06:40 -0400

    adding new multiplier connectivity  (MPC) handler class

---
+++ TicTacToe/MPCHandler.swift
@@ -0,0 +1,16 @@
+//
+//  MPCHandler.swift
+//  TicTacToe
+//
+//  Created by Abhishek Bansal on 2019-07-18.
+//  Copyright © 2019 Abhishek Bansal. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class MPCHandler: NSObject {
+    
+    
+
+}
